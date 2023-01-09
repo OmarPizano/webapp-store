e86c9f076659332786d7bf353507cc2d1cc122c6
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-09 13:48:41 -0600

    Maquetado de la lista de productos.

--- www/public/index.php
+++ www/public/index.php
@@ -41,27 +41,94 @@
                     </div>
                 </aside>
                 <!-- contenido -->
-                <section id="products">
-                    <div class="product">
-                        <img src="assets/logo.png" alt="item">
-                        <h4>Nombre Producto</h4>
-                        <p>$1323.33</p>
-                        <a href="#">Comprar</a>
-                        <a href="#">Carrito</a>
-                    </div>
-                    <div class="product">
-                        <img src="assets/logo.png" alt="item">
-                        <h4>Nombre Producto</h4>
-                        <p>$1323.33</p>
-                        <a href="#">Comprar</a>
-                        <a href="#">Carrito</a>
-                    </div>
-                    <div class="product">
-                        <img src="assets/logo.png" alt="item">
-                        <h4>Nombre Producto</h4>
-                        <p>$1323.33</p>
-                        <a href="#">Comprar</a>
-                        <a href="#">Carrito</a>
+                <section id="content">
+                    <h1>Productos Destacados</h1>
+                    <div id="product-list">
+                        <div class="product">
+                            <img src="assets/logo.png" alt="item">
+                            <div class="product-details">
+                                <div class="product-price">
+                                    <p class="price-current">$1000.00</p>
+                                    <p class="price-discount">-50%</p>
+                                </div>
+                                <p class="price-original">$2000.00</p>
+                                <div class="product-btns">
+                                    <a href="#" class="btn btn-black">Comprar</a>
+                                    <a href="#" class="btn btn-normal">+</a>
+                                </div>
+                                <div class="product-desc">
+                                    <p>Nombre Producto Con Un Texto Largo Larguísimo</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product">
+                            <img src="assets/logo.png" alt="item">
+                            <div class="product-details">
+                                <div class="product-price">
+                                    <p class="price-current">$1000.00</p>
+                                    <p class="price-discount">-50%</p>
+                                </div>
+                                <p class="price-original">$2000.00</p>
+                                <div class="product-btns">
+                                    <a href="#" class="btn btn-black">Comprar</a>
+                                    <a href="#" class="btn btn-normal">+</a>
+                                </div>
+                                <div class="product-desc">
+                                    <p>Nombre Producto Corto</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product">
+                            <img src="assets/logo.png" alt="item">
+                            <div class="product-details">
+                                <div class="product-price">
+                                    <p class="price-current">$1000.00</p>
+                                    <p class="price-discount">-50%</p>
+                                </div>
+                                <p class="price-original">$2000.00</p>
+                                <div class="product-btns">
+                                    <a href="#" class="btn btn-black">Comprar</a>
+                                    <a href="#" class="btn btn-normal">+</a>
+                                </div>
+                                <div class="product-desc">
+                                    <p>Nombre Producto Corto</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product">
+                            <img src="assets/logo.png" alt="item">
+                            <div class="product-details">
+                                <div class="product-price">
+                                    <p class="price-current">$1000.00</p>
+                                    <p class="price-discount">-50%</p>
+                                </div>
+                                <p class="price-original">$2000.00</p>
+                                <div class="product-btns">
+                                    <a href="#" class="btn btn-black">Comprar</a>
+                                    <a href="#" class="btn btn-normal">+</a>
+                                </div>
+                                <div class="product-desc">
+                                    <p>Nombre Producto Corto</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="product">
+                            <img src="assets/logo.png" alt="item">
+                            <div class="product-details">
+                                <div class="product-price">
+                                    <p class="price-current">$1000.00</p>
+                                    <p class="price-discount">-50%</p>
+                                </div>
+                                <p class="price-original">$2000.00</p>
+                                <div class="product-btns">
+                                    <a href="#" class="btn btn-black">Comprar</a>
+                                    <a href="#" class="btn btn-normal">+</a>
+                                </div>
+                                <div class="product-desc">
+                                    <p>Nombre Producto Corto</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
                 <!-- footer -->
