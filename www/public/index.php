4cede2ff0aaa147297f6aa6af4ac8119ba339550
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-18 06:50:37 -0600

    Agregar la clase Request.

--- www/public/index.php
+++ www/public/index.php
@@ -3,6 +3,10 @@ require('../tienda/config/app_config.php');
 require(BASE_DIR . '/tienda/config/db.php');
 require(BASE_DIR . '/vendor/autoload.php');
 
-$name = $_GET['name'] ?? 'World';
+use tienda\core\Request;
 
+$request = new Request;
+$name = $request->query('name', 'World');
+
+// Cambiar por Response
 echo 'Hello ' . $name . '!<br>';
---
+++ www/tienda/core/Request.php
@@ -0,0 +1,79 @@
+<?
+namespace tienda\core;
+
+class Request
+{
+    private array $get;
+    private array $post;
+    private string $method;
+    private string $path;
+
+    public function __construct() {
+        $this->method = $_SERVER['REQUEST_METHOD'] ?? false;
+        $this->path = $this->getPath2();
+        $this->post = $this->getBody('POST');
+        $this->get = $this->getBody('GET');
+    }
+
+    /**
+     * Obtiene un parámetro de GET.
+     * Si no existe, asigna uno por defecto.
+     */
+    public function query(string $key, mixed $default = null) {
+        return $this->get[$key] ?? $default;
+    }
+    
+    /**
+     * Obtiene un parámetro de POST.
+     * Si no existe, asigna uno por defecto.
+     */
+    public function input(string $key, mixed $default = null) {
+        return $this->post[$key] ?? $default;
+    }
+
+    /**
+     * Obtiene el método de la petición actual.
+     * Si por alguna razón no existe, retorna false.
+     */
+    public function getMethod() {
+        return $this->method;
+    }
+
+    /**
+     * Obtiene la ruta de la petición actual sin los parametros GET.
+     */
+    public function getPath() {
+        return $this->path;
+    }
+
+    /**
+     * Genera el path a partir de $_SERVER sin los parámetros GET.
+     */
+    private function getPath2() {
+        $path = $_SERVER['REQUEST_URI'] ?? false;
+        if ($path === false) {
+            return '/';
+        } else {
+            $pos = strpos($path, '?');
+            return ($pos === false) ? $path : substr($path, 0, $pos);
+        }
+    }
+
+    /**
+     * Sanitiza los GET ó POST para inicializarlos.
+     */
+    private function getBody(string $method) {
+        $body = [];
+        if ($method === 'GET') {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($method === 'POST') {
+            foreach ($_POST as $key => $value) {
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        return $body;
+    }
+}
