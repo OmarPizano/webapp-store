bd25429c6522a0a10d7d430fa6653647ac11cf3e
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-21 14:21:55 -0600

    Agregar el formulario de registro.

--- www/public/index.php
+++ www/public/index.php
@@ -7,6 +7,7 @@ require(BASE_DIR . '/vendor/autoload.php');
 use tienda\core\Request;
 use tienda\core\Response;
 
-$view = View::render('product/featured');
+// $view = View::render('product/featured');
+$view = View::render('user/register');
 $response = new Response($view, 200);
 $response->send();
---
+++ www/tienda/views/user/register.php
@@ -0,0 +1,12 @@
+<h1>Nuevo Usuario</h1>
+<?
+use tienda\core\ui\Form;
+
+Form::begin('#', 'POST');
+Form::input('text', 'name', '', 'Nombre de usuario', 'required autofocus');
+Form::input('email', 'email', '', 'Correo electrónico', 'required');
+Form::input('text', 'address', '', 'Domicilio completo', 'required');
+Form::input('password', 'password', '', 'Contraseña', 'required');
+Form::input('password', 'password2', '', 'Verificar contraseña', 'required');
+Form::submit('Enviar');
+Form::end();
