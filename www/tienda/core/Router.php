a4271de37f5092159741297ed9b131547e1fcd3a
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-24 09:07:36 -0600

    Simplificar los comentarios en getCallback()

--- www/tienda/core/Router.php
+++ www/tienda/core/Router.php
@@ -25,44 +25,36 @@ class Router
     }
 
     private static function getCallback(Request $request) {
-        // ver si la ruta solicitada es simple (sin parámetros)
+        // buscar ruta simple
         $callback = self::$routes[$request->getMethod()][$request->getPath()] ?? false;
-        // si la encuentra, la retorna...
         if ($callback != false) {
             return $callback;
         }
-        // ... de lo contrario, ver si se trata de una ruta con parámetros
+        // buscar una ruta con parámetros
         $path = trim($request->getPath(), '/');
-        // obtener todas las rutas del mismo método que el de la petición
         $routes = self::$routes[$request->getMethod()];
-        // recorrer los pares RUTA => callback
         foreach ($routes as $route => $callback) {
             $route = trim($route, '/');
             if (!$route) {
                 continue;   // saltar si la ruta es '/'
             }
             // usar un regex para encontrar los identificadores de la ruta ACTUAL
-            // p. ej. /user/{id}/{mode} -> ['id', 'mode']
             $routeKeys = [];
             if (preg_match_all('/(?<={).+?(?=})/', $route, $matches)) {
                 $routeKeys = $matches[0];
             }
             // crear un regex a partir de la ruta actual
-            // p. ej. /user/{id} -> @^/user/(.+?)$@
             $routeRegex = '@^' . preg_replace('/\{.+?\}/', '(.+?)' ,$route) . '$@';
             // ver si la ruta del request ($path) hace match con el regex de la ruta ACTUAL
             if (preg_match_all($routeRegex, $path, $valueMatches)) {
-                // guardar todos VALORES de la ruta del request
-                // p. ej. /user/{id}/{mode} -> /user/1sf0b6/mobile -> $values = ['1sf0b6', 'mobile'];
+                // guardar los VALORES de la ruta del request
                 $values = [];
                 for ($i = 1; $i < count($valueMatches); $i++) {
                     $values[] = $valueMatches[$i][0];
                 }
                 // combinar los identificadores con los valores encontrados
-                // p. ej. /user/{id}/{mode} -> /user/1sf0b6/mobile -> $params = ['id' => '1sf0b6', 'mode' => 'mobile'];
                 $params = array_combine($routeKeys, $values);
-                // agregar los parametros al Request actual y regresar el callback
-                // de la ruta que hizo match
+                // agregar los parametros al Request y regresar el callback
                 $request->setParams($params);
                 return $callback;
             }
