47ded83ad8c5c6969a7b610dab1eed8cc6f73eed
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-24 12:04:54 -0600

    Refactorizar la clase Request. Unificar los parámetros de GET y POST

--- www/tienda/core/Request.php
+++ www/tienda/core/Request.php
@@ -3,33 +3,23 @@ namespace tienda\core;
 
 class Request
 {
-    private array $get;
-    private array $post;
+    private array $params;
     private string $method;
     private string $path;
 
     public function __construct() {
         $this->method = $_SERVER['REQUEST_METHOD'] ?? false;
-        $this->path = $this->getPath2();
-        $this->post = $this->getBody('POST');
-        $this->get = $this->getBody('GET');
+        $this->path = $this->setPath();
+        $this->params = $this->getParams($this->method);
     }
 
-    // TODO: combinar esta función con getBody()
     /**
      * Agrega parámetros al request actual.
      * Se agregan a POST o GET dependiendo del Request actual.
      */
     public function setParams(array $params) {
-        if ($this->method === 'GET') {
-            foreach ($params as $key => $value) {
-                $this->get[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
-        if ($this->method === 'POST') {
-            foreach ($params as $key => $value) {
-                $this->post[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
+        foreach ($params as $key => $value) {
+            $this->params[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
         }
     }
 
@@ -38,17 +28,9 @@ class Request
      * Si no existe, asigna uno por defecto.
      */
     public function query(string $key, mixed $default = null) {
-        return $this->get[$key] ?? $default;
+        return $this->params[$key] ?? $default;
     }
     
-    /**
-     * Obtiene un parámetro de POST.
-     * Si no existe, asigna uno por defecto.
-     */
-    public function input(string $key, mixed $default = null) {
-        return $this->post[$key] ?? $default;
-    }
-
     /**
      * Obtiene el método de la petición actual.
      * Si por alguna razón no existe, retorna false.
@@ -67,31 +49,24 @@ class Request
     /**
      * Genera el path a partir de $_SERVER sin los parámetros GET.
      */
-    private function getPath2() {
+    private function setPath() {
         $path = $_SERVER['REQUEST_URI'] ?? false;
         if ($path === false) {
             return '/';
-        } else {
-            $pos = strpos($path, '?');
-            return ($pos === false) ? $path : substr($path, 0, $pos);
         }
+        $pos = strpos($path, '?');
+        return ($pos === false) ? $path : substr($path, 0, $pos);
     }
 
     /**
-     * Sanitiza los GET ó POST para inicializarlos.
+     * Retorna los parámetros sanitizados de GET o POST dependiendo de la petición.
      */
-    private function getBody(string $method) {
-        $body = [];
-        if ($method === 'GET') {
-            foreach ($_GET as $key => $value) {
-                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
+    private function getParams(string $method) {
+        $params = [];
+        $config = ['GET' => [$_GET, INPUT_GET], 'POST' => [$_POST, INPUT_POST]];
+        foreach ($config[$method][0] as $key => $value) {
+            $params[$key] = filter_input($config[$method][1], $key, FILTER_SANITIZE_SPECIAL_CHARS);
         }
-        if ($method === 'POST') {
-            foreach ($_POST as $key => $value) {
-                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
-        return $body;
+        return $params;
     }
 }
