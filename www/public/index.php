b93777cf6b0c461722bbb91023ffa34a843348cb
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-01-26 15:31:47 -0600

    Volver a activar las rutas de registro.

--- www/public/index.php
+++ www/public/index.php
@@ -1,18 +1,16 @@
 <?php
-use tienda\controller\TestController;
-use tienda\core\Request;
 require('../tienda/config/app_config.php');
 require(BASE_DIR . '/tienda/config/db.php');
 require(BASE_DIR . '/vendor/autoload.php');
 
 use tienda\core\Router;
+use tienda\core\Request;
 use tienda\controller\ProductController;
 use tienda\controller\UserController;
 
 Router::get('/', [new ProductController, 'featured']);
-// Router::get('/register', [new UserController, 'register']);
-// Router::post('/register', [new UserController, 'register']);
-Router::get('/hello/{name}', [new TestController, 'hello']);
+Router::get('/register', [new UserController, 'register']);
+Router::post('/register', [new UserController, 'register']);
 
 $request = new Request;
 
