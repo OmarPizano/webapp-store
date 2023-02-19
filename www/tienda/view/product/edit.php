8a6213febe7763ce9b1809868d8ee1201ca3ad27
OmarPizano <oj.pizanoorozco@ugto.mx>
2023-02-19 03:54:06 -0600

    Formateada la vista del formulario de edición d eproductos.

--- www/tienda/view/product/edit.php
+++ www/tienda/view/product/edit.php
@@ -2,30 +2,51 @@
 <form action="/product/edit/<?=$model->id?>", method="POST">
     <div class="product-edit-form">
         <div class="photo_box">
-            <div class="photo_box_img">
                 <img src="<?= ASSET_URL . $model->product_image ?>" alt="product">
-            </div>
             <div class="photo_box_button">
-                <input type="file", name='product_image', accept="image/png">
+                <label>Dejar la imagen actual o seleccionar una nueva
+                <input type="file", name='product_image', accept="image/png"></label>
             </div>
         </div>
         <div class="data_box">
-            <input type="text" name="product_name" value="<?=$model->product_name?>" placeholder="Nombre del producto" required autofocus>
-            <textarea name="product_description" value="<?=$model->product_description?>">Descripción del producto</textarea>
-            <!-- TODO: autocargar las categorias de la dn -->
-            <select name="product_category">
-                <!-- <option value="" selected="selected" hidden="hidden"><?=$model->category_id?></option> -->
-                <option value="0001">Ropa</option>
-                <option value="0002">Supermercado</option>
-                <option value="0003">Tecnología</option>
-            </select>
-            <input type="number" name="product_stock" value="<?=$model->product_stock?>" placeholder="Existencias" required>
-            <input type="number" name="product_price" value="<?=$model->product_price?>" placeholder="Precio neto" required>
-            <input type="number" name="product_discount" value="<?=$model->product_discount?>" placeholder="Descuento a aplicar" required>
+            <div class="text_data">
+                <label>Nombre
+                    <input type="text" name="product_name" value="<?=$model->product_name?>" required autofocus>
+                </label>
+                <label>Descripción
+                <textarea name="product_description" ><?= $model->product_description ?></textarea>
+                </label>
+                <!-- TODO: autocargar las categorias de la dn -->
+                <label>Categoría
+                    <select name="product_category">
+                        <option value="0001">Ropa</option>
+                        <option value="0002">Supermercado</option>
+                        <option value="0003">Tecnología</option>
+                    </select>
+                </label>
+            </div>
+            <div class='numeric_data'>
+                <div>
+                    <label>Stock
+                    <input type="number" name="product_stock" value="<?=$model->product_stock?>" required></label>
+                </div>
+                <div>
+                    <label>Precio
+                    <input type="number" name="product_price" value="<?=$model->product_price?>" required></label>
+                </div>
+                <div>
+                    <label>Descuento
+                    <input type="number" name="product_discount" value="<?=$model->product_discount?>" required></label>
+                </div>
+            </div>
         </div>
         <div class="submit_box">
-            <input type="submit" name="edit_product_submit" value="Confirmar">
-            <a href="/product/admin">Cancelar</a>
+            <div>
+                <input type="submit" name="edit_product_submit" value="Confirmar">
+            </div>
+            <div>
+                <a href="/product/admin">Cancelar</a>
+            </div>
         </div>
     </div>
 </form>
